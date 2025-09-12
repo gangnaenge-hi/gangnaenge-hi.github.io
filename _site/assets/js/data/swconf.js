9477ace2fede8c6e3a80d7cbf201072c5d447b1b
gangnaenge-hi <gangnaenge.hi@gmail.com>
2025-09-12 15:35:13 +0900

    update post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1757658057',resources: [
+    cacheName: 'chirpy-1757658755',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
