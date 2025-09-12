30eb2f25a8d963fcb4b86783b10fa300c691c1c1
gangnaenge-hi <gangnaenge.hi@gmail.com>
2025-09-12 15:39:52 +0900

    이미지 디렉토리 변경

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1757658755',resources: [
+    cacheName: 'chirpy-1757659122',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
